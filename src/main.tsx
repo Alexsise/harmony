58d4a78a9ae63b7c5369a8fc06b8fbeefad7ae23
(Ca)бля <alexsise.ur@gmail.com>
2025-04-11 03:08:29 +0300

    Rename css stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
-import "./index.css";
 import App from "./App.tsx";
+import "./main.css";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
