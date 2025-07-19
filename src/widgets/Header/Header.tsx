b05e8c4e9cde52f61d7c9ef671a8481ed038c233
(Ca)бля <alexsise.ur@gmail.com>
2025-07-19 22:21:05 +0300

    refactor(widgets): update "Header" to use CSS Modules

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,12 +1,13 @@
-import "./Header.css";
-import Package from "../../../package.json";
+import styles from "./Header.module.css";
+
+import Package from "@/../package.json";
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="title">
-        <span className="name">Harmony </span>
-        <span className="version">v{Package.version}</span>
+    <header className={styles.header}>
+      <div className={styles.title}>
+        <span className={styles.name}>Harmony </span>
+        <span className={styles.version}>v{Package.version}</span>
       </div>
     </header>
   );
