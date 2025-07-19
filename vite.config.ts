040cc0d92180bf325afd231f6098bfd4c786f5ae
(Ca)бля <alexsise.ur@gmail.com>
2025-07-19 22:04:37 +0300

    chore(typescript): add "vite-tsconfig-paths" to use with tsconfig "paths"

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsConfigPaths from 'vite-tsconfig-paths'
 import svgr from "vite-plugin-svgr";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), tsConfigPaths()],
   resolve: {
     alias: {
       "@icons": path.resolve(__dirname, "src", "assets"),
